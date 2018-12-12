fa09f311c6cc623d49f21bcfd5f8c7fea4e3ea62
Bayi <bayi@bayi.hu>
2018-12-12 22:42:20 +0100

    New version

--- index.js
+++ index.js
@@ -3,14 +3,31 @@
 var os          = require('os')
 var disk        = require('diskusage')
 var mqtt        = require('mqtt')
+var machineId   = require('node-machine-id')
+var hostname    = `host${capitalize(os.hostname())}`
 
-const root = `sysinfo/${os.hostname()}`
+const root = `homeassistant/sensor/${hostname}`
+const state = `${root}/state`
+const config = `${root}/config`
+const configObj = {
+    name: hostname,
+    icon: 'mdi:laptop',
+    json_attributes: [ 'status', 'diskpercent', 'diskfree', 'uptime', 'mempercent', 'memfree' ],
+    value_template: '{{ value_json.status }}',
+    unique_id: machineId.machineIdSync(),
+}
 
 process.on('exit', () => {
     console.log('Kilepes ...')
 })
 
-function abort(str) {
+function capitalize(str)
+{
+    return str.charAt(0).toUpperCase() + str.slice(1)
+}
+
+function abort(str)
+{
     console.error(str)
     process.exit(1)
 }
@@ -18,18 +35,18 @@ function abort(str) {
 console.log('Csatlakozas a brokerhez ...')
 const mqttOptions = {}
 mqttOptions.will = {
-    topic: `${root}/presence`,
-    payload: '0',
+    topic: state,
+    payload: '{"status": "offline"}',
     qos: 2,
     retain: true
 }
 
 const client = mqtt.connect('mqtt://mqtt.bayi.hu', mqttOptions)
-client.subscribe(`${root}/presence`)
+client.subscribe(config)
 
 client.on('connect', () => {
     console.log('Csatlakozva.')
-    client.publish(`${root}/presence`, '1', { retain: true })
+    client.publish(config, JSON.stringify(configObj), { retain: true })
 })
 
 
@@ -37,19 +54,20 @@ function getStatus()
 {
     return new Promise(resolve => {
         const info = {
+            status: "online",
             disktotal: 0,
             diskfree: 0,
             diskpercent: 0.00,
             loadavg: os.loadavg(),
             uptime: os.uptime(),
-            memtotal: os.totalmem(),
-            memfree: os.freemem(),
-            mempercent: ((os.freemem() / os.totalmem()) * 100).toFixed(2)
+            memtotal: parseFloat(os.totalmem()),
+            memfree: parseFloat(os.freemem()),
+            mempercent: parseFloat(((os.freemem() / os.totalmem()) * 100).toFixed(2))
         }
         disk.check('/', (err, diskinfo) => {
             info.disktotal = diskinfo.total
             info.diskfree = diskinfo.free
-            info.diskpercent = ((diskinfo.free / diskinfo.total) * 100).toFixed(2)
+            info.diskpercent = parseFloat(((diskinfo.free / diskinfo.total) * 100).toFixed(2))
             resolve(info)
         })
     })
@@ -58,7 +76,7 @@ function getStatus()
 function publish()
 {
     getStatus().then(info => {
-        client.publish(`${root}/status`, JSON.stringify(info), { retain: true })
+        client.publish(state, JSON.stringify(info), { retain: true })
     })
 }
 
